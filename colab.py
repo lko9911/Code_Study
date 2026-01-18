80cc5687733c53fc1cde72ebac37a0860731cd81
Lee Gyuwon <lko991111@gmail.com>
2026-01-18 13:26:45 +0900

    260118 업데이트 - 구글 마운트 코드 추가

--- colab.py
+++ colab.py
@@ -2,3 +2,23 @@
 
 from google.colab import drive
 drive.mount('/content/drive')
+
+# 정보 저장 코드 (드라이브)
+
+import shutil
+import os
+
+# 원본 경로
+src = ""
+
+# Drive에 저장될 경로 (이름 지정)
+dst = "/content/drive/MyDrive/"
+
+# 이미 존재하면 삭제 (선택)
+if os.path.exists(dst):
+    shutil.rmtree(dst)
+
+# 복사
+shutil.copytree(src, dst)
+
+print("구글 드라이브 저장 완료:", dst)
